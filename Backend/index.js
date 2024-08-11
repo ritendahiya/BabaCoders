40dd9fef3231e93d93ca12e29c86223145492602
Pankaj Kumar <pankajbaliyan90@gmail.com>
2024-08-11 23:19:33 +0530

    update code

--- Backend/index.js
+++ Backend/index.js
@@ -1,32 +1,25 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const userRoutes = require('./routes/user');
-const appointmentRoutes = require('./routes/appointment');
-const paymentRoutes = require('./routes/payment');
-const feedbackRoutes = require('./routes/feedback');
-require('dotenv').config();
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
+// const userRoutes = require("./routes/user");
+// const appointmentRoutes = require("./routes/appointment");
+// const paymentRoutes = require("./routes/payment");
+// const feedbackRoutes = require("./routes/feedback");
 const app = express();
+app.use(cors());
+const sqlDBConnect = require("./config/sql_connect");
+const authRouter = require("./routes/01_userRoute");
 
 app.use(express.json());
 
-// Connect to MongoDB
-// mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-//   .then(() => console.log('MongoDB connected'))
-//   .catch(err => console.log(err));
-
-
-
-// Root route
-app.get('/', (req, res) => {
-  res.send('Welcome to the Patient Management System API!');
-});
-
 // Use routes
-app.use('/api/users', userRoutes);
-app.use('/api/appointments', appointmentRoutes);
-app.use('/api/payments', paymentRoutes);
-app.use('/api/feedback', feedbackRoutes);
+app.use("/api/auth", authRouter);
+// app.use('/api/users', userRoutes);
+// app.use('/api/appointments', appointmentRoutes);
+// app.use('/api/payments', paymentRoutes);
+// app.use('/api/feedback', feedbackRoutes);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
